d3272cacff1173be8a5a14781e334472681bf420
Elissaios <elissaios@MacBook-Air.local>
2022-09-07 04:43:25 +0300

    Updates

---
+++ precache-manifest.8df131e017a7f6f0f5b6aa735d245a35.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "8c30221281b4ee956d7d",
+    "url": "/Portfolio/static/css/main.7d58adaa.chunk.css"
+  },
+  {
+    "revision": "8c30221281b4ee956d7d",
+    "url": "/Portfolio/static/js/main.8c302212.chunk.js"
+  },
+  {
+    "revision": "6a7bfa4df9242b40edff",
+    "url": "/Portfolio/static/js/1.6a7bfa4d.chunk.js"
+  },
+  {
+    "revision": "0b87f2d90c37382afbe1",
+    "url": "/Portfolio/static/js/2.0b87f2d9.chunk.js"
+  },
+  {
+    "revision": "939327d006d468fc6e05",
+    "url": "/Portfolio/static/js/runtime~main.939327d0.js"
+  },
+  {
+    "revision": "33cb98c77c51c6dd9f49d838455e5467",
+    "url": "/Portfolio/static/media/p_2.33cb98c7.jpeg"
+  },
+  {
+    "revision": "9618ead0ef4877542c7680e956acb217",
+    "url": "/Portfolio/index.html"
+  }
+];
